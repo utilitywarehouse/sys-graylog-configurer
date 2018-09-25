c08d9e06d593658225c9361bf841294280de9cb8
Hector Huertas <hectorhuertas@gmail.com>
2018-09-25 18:24:36 +0200

    Absract input creation and add support for cloudtrail dev

--- run.sh
+++ run.sh
@@ -3,59 +3,54 @@ until curl -s "http://127.0.0.1:9000/api/system/lbstatus"; do
   echo 'graylog not ready, sleeping for 3 seconds'
   sleep 3
 done
+printf "\n"
 
 graylog_api="http://admin:${ADMIN_PASSWORD}@127.0.0.1:9000/api"
 
-if [ "$AWS_ENABLED" = "true" ]; then
-  printf "\nSetup aws configuration\n"
-  aws_plugin_config='{"lookup_regions":"eu-west-1", "access_key": "'"${AWS_ACCESS_KEY_ID}"'", "secret_key": "'"${AWS_SECRET_ACCESS_KEY}"'"}'
-  curl -s -X PUT -H "Content-Type: application/json" -d "${aws_plugin_config}" "${graylog_api}/system/cluster_config/org.graylog.aws.config.AWSPluginConfiguration"
+set_input()
+{
+  input_name="$1"
+  input_data="$2"
 
-  printf "\nSetup aws input\n"
-  aws_id=$(curl -s -XGET "${graylog_api}/system/inputs" | jq -r '.inputs[] | select(.title == "aws_cloudtrail_input") | .id')
-  aws_input='{"title":"aws_cloudtrail_input","type":"org.graylog.aws.inputs.cloudtrail.CloudTrailInput","configuration":{"aws_sqs_region":"eu-west-1","aws_s3_region":"eu-west-1","aws_sqs_queue_name":"'"${AWS_SQS_QUEUE_NAME}"'"},"global":true}'
-  if [ ! "$aws_id" ]; then
-    curl -s -X POST -H "Content-Type: application/json" -d "${aws_input}" "${graylog_api}/system/inputs"
-    printf "\nAWS input created\n"
+  printf "\nSetup ${input_name}\n"
+
+  input_id=$(curl -s -XGET "${graylog_api}/system/inputs" | jq -r '.inputs[] | select(.title == "'"${input_name}"'") | .id')
+
+  if [ ! "${input_id}" ]; then
+    curl -s -X POST -H "Content-Type: application/json" -d "${input_data}" "${graylog_api}/system/inputs"
+    printf "\n${input_name} created\n"
   else
-    curl -s -X PUT -H "Content-Type: application/json" -d "${aws_input}" "${graylog_api}/system/inputs/${aws_id}"
-    echo "\nAWS input updated\n"
+    curl -s -X PUT -H "Content-Type: application/json" -d "${input_data}" "${graylog_api}/system/inputs/${input_id}"
+    printf "\n${input_name} updated\n"
   fi
+}
+
+if [ "${AWS_CLOUDTRAIL_PROD_ENABLED}" = "true" ]; then
+  input_name=aws_cloudtrail_input_prod
+  input_data='{"title":"'"${input_name}"'","type":"org.graylog.aws.inputs.cloudtrail.CloudTrailInput","configuration":{"aws_sqs_region":"eu-west-1","aws_s3_region":"eu-west-1","aws_sqs_queue_name":"'"${AWS_SQS_QUEUE_PROD}"'","aws_access_key":"'"${AWS_ID_PROD}"'","aws_secret_key":"'"${AWS_SECRET_PROD}"'"},"global":true}'
+  set_input ${input_name} ${input_data}
 fi
 
-printf "\nSetup udp input\n"
-udp_id=$(curl -s -XGET "${graylog_api}/system/inputs" | jq -r '.inputs[] | select(.title == "gelf_udp_input") | .id')
-udp_input='{"title":"gelf_udp_input","type":"org.graylog2.inputs.gelf.udp.GELFUDPInput","configuration":{"port":12201,"bind_address":"0.0.0.0"},"global":true}'
-if [ ! "$udp_id" ]; then
-  curl -s -X POST -H "Content-Type: application/json" -d "${udp_input}" "${graylog_api}/system/inputs"
-  printf "\nUDP input created\n"
-else
-  curl -s -X PUT -H "Content-Type: application/json" -d "${udp_input}" "${graylog_api}/system/inputs/${udp_id}"
-  printf "\nUDP input updated\n"
+if [ "${AWS_CLOUDTRAIL_DEV_ENABLED}" = "true" ]; then
+  input_name=aws_cloudtrail_input_dev
+  input_data='{"title":"'"${input_name}"'","type":"org.graylog.aws.inputs.cloudtrail.CloudTrailInput","configuration":{"aws_sqs_region":"eu-west-1","aws_s3_region":"eu-west-1","aws_sqs_queue_name":"'"${AWS_SQS_QUEUE_DEV}"'","aws_access_key":"'"${AWS_ID_DEV}"'","aws_secret_key":"'"${AWS_SECRET_DEV}"'"},"global":true}'
+  set_input ${input_name} ${input_data}
 fi
 
-printf "\nSetup tcp input\n"
-tcp_id=$(curl -s -XGET "${graylog_api}/system/inputs" | jq -r '.inputs[] | select(.title == "gelf_tcp_input") | .id')
-tcp_input='{"title":"gelf_tcp_input","type":"org.graylog2.inputs.gelf.tcp.GELFTCPInput","configuration":{"port":12202,"bind_address":"0.0.0.0"},"global":true}'
-if [ ! "$tcp_id" ]; then
-  curl -s -X POST -H "Content-Type: application/json" -d "${tcp_input}" "${graylog_api}/system/inputs"
-  printf "\nUDP input created\n"
-else
-  curl -s -X PUT -H "Content-Type: application/json" -d "${tcp_input}" "${graylog_api}/system/inputs/${tcp_id}"
-  printf "\nUDP input updated\n"
-fi
+input_data='{"title":"gelf_tcp_input","type":"org.graylog2.inputs.gelf.tcp.GELFTCPInput","configuration":{"port":12202,"bind_address":"0.0.0.0"},"global":true}'
+set_input gelf_tcp_input ${input_data}
 
 printf "\nSetup kubernetes extractor\n"
-udp_id=$(curl -s -XGET "${graylog_api}/system/inputs" | jq -r '.inputs[] | select(.title == "gelf_udp_input") | .id')
-k8s_extractor_id=$(curl -s -XGET "${graylog_api}/system/inputs/${udp_id}/extractors" | jq -r '.extractors[] | select(.title == "kubernetes") | .id')
+tcp_input_id=$(curl -s -XGET "${graylog_api}/system/inputs" | jq -r '.inputs[] | select(.title == "gelf_tcp_input") | .id')
+k8s_extractor_id=$(curl -s -XGET "${graylog_api}/system/inputs/${tcp_input_id}/extractors" | jq -r '.extractors[] | select(.title == "kubernetes") | .id')
 k8s_extractor='{"title":"kubernetes","cut_or_copy":"copy","source_field":"kubernetes","extractor_type":"json","target_field":"","extractor_config":{"key_prefix":"k8s_"},"converters":{},"condition_type":"none","condition_value":""}'
 
-if [ ! "$k8s_extractor_id" ]; then
-  curl -s -X POST -H "Content-Type: application/json" -d "${k8s_extractor}" "${graylog_api}/system/inputs/${udp_id}/extractors"
-  printf "\nK8s extractor created\n"
+if [ ! "${k8s_extractor_id}" ]; then
+  curl -s -X POST -H "Content-Type: application/json" -d "${k8s_extractor}" "${graylog_api}/system/inputs/${tcp_input_id}/extractors"
+  printf "\nk8s extractor created\n"
 else
-  curl -s -X PUT -H "Content-Type: application/json" -d "${k8s_extractor}" "${graylog_api}/system/inputs/${udp_id}/extractors/${k8s_extractor_id}"
-  printf "\nK8s extractor updated\n"
+  curl -s -X PUT -H "Content-Type: application/json" -d "${k8s_extractor}" "${graylog_api}/system/inputs/${tcp_input_id}/extractors/${k8s_extractor_id}"
+  printf "\nk8s extractor updated\n"
 fi
 
 printf "\nSetup SSO plugin\n"
